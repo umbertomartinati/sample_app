4e069046e9f1a0146624d271a92f3c5015e2d03a
umbertomartinati <ugomarti@gmail.com>
2015-02-27 13:29:10 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+
+  get 'pages/home'
+
+
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
