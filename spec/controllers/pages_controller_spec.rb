8d77f4fc584977f046c3169babbab0496df39378
umbertomartinati <ugomarti@gmail.com>
2015-03-19 15:58:40 +0100

    Lay1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe PagesController, type: :controller do
   
   it "should have the right title" do
     get 'home'
-    response.should have_selector("title",:content => "Ruby on Rails Tutorial Sample App | Home")
+    response.should have_selector("title:contains('Ruby on Rails Tutorial Sample App | Home')")
   end
   end
   
